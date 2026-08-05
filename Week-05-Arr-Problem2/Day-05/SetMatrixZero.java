aec3613fed32e2819edbc5a7acb7b0aa91ccca65
Virender Naphriya <virendernaphriya689@gmail.com>
2026-08-05 06:13:25 +0530

    solve the setMatrix zero with one more brute that works for +ve only and then do a better solution

--- Week-05-Arr-Problem2/Day-05/SetMatrixZero.java
+++ Week-05-Arr-Problem2/Day-05/SetMatrixZero.java
@@ -28,9 +28,76 @@ public class SetMatrixZero {
         }
     }
 
+
+    //worked for non negative nums only
+    static void setMatrixZeroBrute2(int arr[][]){
+        int m=arr.length;
+        int n=arr[0].length;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j]==0){
+                    markRow(i,arr);
+                    markCol(j,arr);
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j]==-1){
+                    arr[i][j]=0;
+                }
+            }
+        }
+
+    }
+
+    static void markRow(int row,int arr[][]){
+        int n= arr[0].length;
+        for(int i=0; i<n; i++){
+            if(arr[row][i]!=0){
+                arr[row][i]=-1;
+            }
+        }
+    }
+    static void markCol(int col,int arr[][]){
+        int m= arr.length;
+        for(int i=0; i<m; i++){
+            if(arr[i][col]!=0){
+                arr[i][col]=-1;
+            }
+        }
+    }
+
+
+    static void setMatrixZeroBetter(int arr[][]){
+        int m=arr.length;
+        int n= arr[0].length;
+
+        int row[] = new int[n];
+        int col[]= new int[m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(arr[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i]==1 || col[j]==1){
+                    arr[i][j]=0;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[][]={{1,1,1},{1,0,1},{1,1,1}};
-        setMatrixZeroBrute(arr);
+        setMatrixZeroBetter(arr);
 
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
